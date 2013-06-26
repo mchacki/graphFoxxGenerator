15606e1f9ea72f95ec772942933380ea1a23c277
Hacki <michael.hackstein@rwth-aachen.de>
2013-06-26 15:38:58 +0200

    Initial can now be defined as well

--- assets/js/views/changeInitialView.js
+++ assets/js/views/changeInitialView.js
@@ -5,29 +5,26 @@ $(function () {
 
 	app.ChangeInitialView = Backbone.View.extend({
     
-    template: new EJS({url: 'templates/changeLayoutOpticView.ejs'}),
+    template: new EJS({url: 'templates/changeInitialView.ejs'}),
     
     el: "#content",
     
 		events: {
-      "submit form#layout": "submitOptic"
+      "submit form#initial": "submitInitial"
     },
     
-    submitOptic: function (event) {
+    submitInitial: function (event) {
       var res = {};
-      var g = $("#gravity").val();
-      var d = $("#distance").val();
-      var c = $("#charge").val();
-      res.type = "force"; // Hardcoded due to no alternative
-      res.gravity = parseFloat(g) || null;
-      res.distance = parseFloat(d) || null;
-      res.charge = (-1 * parseFloat(c)) || null;
-      app.connection.updateConfig("layouter", res);
+      $("input:radio").map(function() {
+        res[this.id] = this.checked;
+      });
+      delete res.none;
+      app.connection.updateConfig("actions", res);
       event.stopPropagation();
     },
     
 		render: function () {
-      var name = "layouter";
+      var name = "actions";
 			var self = this;
 			app.connection.getConfigInfo(name, function(data) {
         $(self.el).html(self.template.render({data: data}));
@@ -35,6 +32,5 @@ $(function () {
       return this;
 		}
 		
-    
 	});
 }());
